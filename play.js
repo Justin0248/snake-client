4ea2510c5532113287a5046cb7888daf1594ffc8
Yonin408 <justin12689@gmail.com>
2023-03-15 21:23:27 -0700

    completed connect to server assignment

---
+++ play.js
@@ -0,0 +1,16 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log('connected')
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
