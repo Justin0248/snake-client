68afa9340ab4e4c18f74881c04c65eccca8b4406
Yonin408 <justin12689@gmail.com>
2023-03-16 00:09:48 -0700

    completed stdin setup assignment

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
 const net = require("net");
 const {connect, conn, host, port} = require('./client.js');
+
+connect() 
 conn({
 host,
 port
 });
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (handleUserInput) => {
+    if (handleUserInput === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+setupInput();
